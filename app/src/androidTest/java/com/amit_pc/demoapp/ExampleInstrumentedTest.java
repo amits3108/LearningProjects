d0474d2a565a6f960f37a139105f78e72a4567e8
amits3108 <amits3108@gmail.com>
2017-12-18 00:18:55 +0530

    junit testing in demoapp.

--- app/src/androidTest/java/com/amit_pc/demoapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/amit_pc/demoapp/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.amit_pc.demoapp", appContext.getPackageName());
+        assertEquals("com.amit_pc.demoapp", appContext.getPackageName());
     }
 }
